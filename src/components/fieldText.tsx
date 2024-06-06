c89e15bbe8f569bf20d1beec9f62dc27aa9d0f4c
rom013 <romullomelo0130@gmail.com>
2024-06-06 19:51:23 -0300

    fix: name project

--- src/components/fieldText.tsx
+++ src/components/fieldText.tsx
@@ -10,40 +10,47 @@ function FieldText({ data, idField }: FieldProps) {
     function handleDeletedWork(e: ChangeEvent<HTMLInputElement>) {
         setWord(e.target.value.toLowerCase())
 
-        data.attemptList[idField] = { letter: e.target.value.toLowerCase(), position: idField }
+        data.attemptList[idField] = {
+            letter: e.target.value.toLowerCase(),
+            position: idField
+        }
 
         setIsDeleted(false)
         data.setAttempt(data.attemptList)
     }
 
-    return data.status ? (<input
-        type="text"
-        className="square-field"
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            if (e.target.value.trim() === '') {
-                setWord(e.target.value.toLowerCase())
-                return setIsDeleted(true)
-            }
+    return (
+        data.status ? (
+            <input
+                type="text"
+                className="square-field"
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    if (e.target.value.trim() === '') {
+                        setWord(e.target.value.toLowerCase())
+                        return setIsDeleted(true)
+                    }
 
-            if (isDeleted) {
-                return handleDeletedWork(e)
-            }
+                    if (isDeleted) {
+                        return handleDeletedWork(e)
+                    }
 
-            if (word.length < 1) {
-                setWord(e.target.value.toLowerCase())
-                const obj = { letter: e.target.value.toLowerCase(), position: idField }
-                data.setAttempt([...data.attemptList, obj])
-            }
-        }}
-        value={word}
-    />)
-        : (
-            <div
-                className={twMerge("box", data.result[idField])}
-            >
-                <span>{word}</span>
-            </div>
+                    if (word.length < 1) {
+                        setWord(e.target.value.toLowerCase())
+                        const obj = { letter: e.target.value.toLowerCase(), position: idField }
+                        data.setAttempt([...data.attemptList, obj])
+                    }
+                }}
+                value={word}
+            />
         )
+            : (
+                <div
+                    className={twMerge("box", data.result[idField])}
+                >
+                    <span>{word}</span>
+                </div>
+            )
+    )
 }
 
 const memoFieldText = memo(FieldText)
